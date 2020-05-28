cb46d1c1340581ffd6b37823b5634d8de32373b4
JoakimBrandt <brjo1621@student.ju.se>
2020-05-28 11:09:03 +0200

    Adds change to Database
    Changed so elements of type Int are now String

--- database/initialize-database.sql
+++ database/initialize-database.sql
@@ -1,23 +1,23 @@
 CREATE TABLE functions (
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(45) NOT NULL,
-  amount INT NOT NULL,
+  amount VARCHAR(45) NOT NULL,
   CONSTRAINT functionNameUnique UNIQUE (name)
 );
 
 CREATE TABLE resources (
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(45) NOT NULL,
-  amount INT NOT NULL,
+  amount VARCHAR(45) NOT NULL,
   CONSTRAINT resourceNameUnique UNIQUE (name)
 );
 
 CREATE TABLE servants (
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(45) NOT NULL,
-  salary INT NOT NULL,
-  amount INT NOT NULL,
-  production_amount INT,
+  salary VARCHAR(45) NOT NULL,
+  amount VARCHAR(45) NOT NULL,
+  production_amount VARCHAR(45),
   production_type VARCHAR(45),
   CONSTRAINT servantNameUnique UNIQUE (name)
 );
